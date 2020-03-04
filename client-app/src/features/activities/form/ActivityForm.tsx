3f19c5aa8450f5f73369f448c4f89e7fe59b0010
Dean Phillips <deanphillips@corelogic.com>
2020-03-04 06:56:15 -0600

    added cancel and submit buttons to edit activity form

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import { Segment, Form } from 'semantic-ui-react'
+import React from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
 
 export const ActivityForm = () => {
-    return (
-        <Segment>
-            <Form>
-                <Form.Input placeholder="Title"></Form.Input>
-                <Form.TextArea placeholder="Description" rows={2}></Form.TextArea>
-                <Form.Input placeholder="Category"></Form.Input>
-                <Form.Input type="date"></Form.Input>
-                <Form.Input placeholder="City"></Form.Input>
-                <Form.Input placeholder="Venue"></Form.Input>
-            </Form>
-        </Segment>
-    )
-}
+  return (
+    <Segment clearing>
+      <Form>
+        <Form.Input placeholder="Title"></Form.Input>
+        <Form.TextArea placeholder="Description" rows={2}></Form.TextArea>
+        <Form.Input placeholder="Category"></Form.Input>
+        <Form.Input type="date"></Form.Input>
+        <Form.Input placeholder="City"></Form.Input>
+        <Form.Input placeholder="Venue"></Form.Input>
+        <Button
+          floated="right"
+          positive
+          type="submit"
+          content="Submit"
+        ></Button>
+        <Button floated="right" type="button" content="Cancel"></Button>
+      </Form>
+    </Segment>
+  );
+};
